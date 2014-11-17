758dfce8e20a4208e569915ee1af564c51af425a
Alexis Matelin <a.matelin@gmail.com>
2014-11-17 19:17:21 +0630

    Commit 8

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var mongo = require('mongodb');
+var async = require('async');
  
 var Server = mongo.Server,
 Db = mongo.Db,
@@ -17,9 +18,9 @@ db.open(function(err, db) {
 		});
 	}
 });
-
+/*
 exports.render = function(req, res) {
-	data = []
+	var data = {}
 	db.collection('profiles', function(err, collection) {
 		collection.count(function(err, nbr) {
 			console.log("Profiles processed : "+nbr);
@@ -47,5 +48,47 @@ exports.render = function(req, res) {
 			});
 		});
 	})			
-};
+};*/
 
+exports.render = function(req, res) {
+	var data = {};
+	async.parallel([
+		//Get number of profiles processed
+		function(callback){
+			db.profiles.count(function(err, res){
+				if (err) return callback(err);
+				data.nbr_profiles_processed = res;
+				callback();
+			});
+		},
+		//Get number streets processed
+		function(callback){
+			db.complete_streets.find({"profiles_processed":1}).count(function(err, res){
+				if (err) return callback(err);
+				data.nbr_streets_processed = res;
+				callback();
+			});
+		},
+		//Get number streets remaining 
+		function(callback){
+			db.complete_streets.find({"profiles_processed":0}).count(function(err, res){
+				if (err) return callback(err);
+				data.nbr_streets_remaining = res;
+				callback();
+			});
+		},
+		//Get number of errors
+		function(callback){
+			db.complete_streets.find({"profiles_processed":-1}).count(function(err, res){
+				if (err) return callback(err);
+				data.nbr_errors = res;
+				callback();
+			});
+		}
+	],	 
+		//Callback function, executed after all the async calls are done with 
+		function(err){
+			if (err) return next(err); //Let express handle the error 
+			res.render('Index', {title:'Scraper Monitor', data:data});
+	});
+};
