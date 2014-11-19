1ec128591610c674bffea366b544410fb979a103
Alexis Matelin <a.matelin@gmail.com>
2014-11-18 18:22:28 -0800

    Commit 10

--- routes/index.js
+++ routes/index.js
@@ -18,77 +18,70 @@ db.open(function(err, db) {
 		});
 	}
 });
-/*
-exports.render = function(req, res) {
-	var data = {}
-	db.collection('profiles', function(err, collection) {
-		collection.count(function(err, nbr) {
-			console.log("Profiles processed : "+nbr);
-			data.push(nbr);
-			db.collection('complete_streets', function(err, collection) {
-				collection.find({"profiles_processed":1}).count(function(err, nbr) {
-					console.log("Nbr streets processed : "+nbr);
-					data.push(nbr);
-					db.collection('complete_streets', function(err, collection) {
-						collection.find({"profiles_processed":0}).count(function(err, nbr) {
-							console.log("Nbr streets to process : "+nbr);
-							data.push(nbr);
-							db.collection('complete_streets', function(err, collection) {
-								collection.find({"profiles_processed":-1}).count(function(err, nbr) {
-									console.log("Nbr streets with error : "+nbr);
-									data.push(nbr);
-									var nbr_profiles = data[0]
-									var percent = Math.round((data[1]/data[2])*100)			
-									res.render('Index', {title:'Scraper Monitor', figures:data, percentage:percent, nbr_profiles:nbr_profiles});						
-								});
-							});
-						});
-					});
-				});
-			});
-		});
-	})			
-};*/
+
 
 exports.render = function(req, res) {
 	var data = {};
 	async.parallel([
 		//Get number of profiles processed
 		function(callback){
-			db.profiles.count(function(err, res){
-				if (err) return callback(err);
-				data.nbr_profiles_processed = res;
-				callback();
+			db.collection('profiles', function(err, collection) {
+				collection.count(function(err, res){
+					if (err) return callback(err);
+					data.nbr_profiles_processed = res;
+					callback();
+				});
 			});
+			
 		},
 		//Get number streets processed
 		function(callback){
-			db.complete_streets.find({"profiles_processed":1}).count(function(err, res){
+			db.collection('complete_streets', function(err, collection) {
+				collection.find({"profiles_processed":1}).count(function(err, res){
 				if (err) return callback(err);
 				data.nbr_streets_processed = res;
 				callback();
+				});
 			});
+
 		},
 		//Get number streets remaining 
 		function(callback){
-			db.complete_streets.find({"profiles_processed":0}).count(function(err, res){
-				if (err) return callback(err);
-				data.nbr_streets_remaining = res;
-				callback();
+			db.collection('complete_streets', function(err, collection) {
+				collection.find({"profiles_processed":0}).count(function(err, res){
+					if (err) return callback(err);
+					data.nbr_streets_remaining = res;
+					callback();
+				});	
 			});
+
 		},
 		//Get number of errors
 		function(callback){
-			db.complete_streets.find({"profiles_processed":-1}).count(function(err, res){
-				if (err) return callback(err);
-				data.nbr_errors = res;
-				callback();
+			db.collection('complete_streets', function(err, collection) {
+				collection.find({"profiles_processed":-1}).count(function(err, res){
+					if (err) return callback(err);
+					data.nbr_errors = res;
+					callback();
+				});
 			});
+			
 		}
 	],	 
 		//Callback function, executed after all the async calls are done with 
 		function(err){
 			if (err) return next(err); //Let express handle the error 
-			res.render('Index', {title:'Scraper Monitor', data:data});
+			var status;
+			if (data.nbr_errors>1000) {
+				status=0;
+			} else {
+				status=1;
+			}
+			var progress_ratio = Math.round((data.nbr_streets_processed/(data.nbr_streets_remaining+data.nbr_streets_processed+data.nbr_errors))*100);
+			res.render('Index', {title:'Scraper Monitor', 
+					  			 mongo_data:data, 
+					  			 nbr_profiles_processed:data.nbr_profiles_processed, 
+					  			 percentage:progress_ratio, 
+					  			 status:status});
 	});
 };
